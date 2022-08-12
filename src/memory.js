e49ad42b89f850e1243d7c53dec3e723351a0180
Valeria <valeriaaidoc@gmail.com>
2022-08-12 11:29:07 +0200

    finish exercise

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,34 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards.length) return undefined;
+    let i = this.cards.length;
+    while (--i > 0) {
+      let randIndex = Math.floor(Math.random() * (i + 1));
+      [this.cards[randIndex], this.cards[i]] = [
+        this.cards[i],
+        this.cards[randIndex],
+      ];
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    const card = card1 == card2;
+
+    if (card) this.pairsGuessed += 1;
+    return card;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsClicked == 0) return false;
+
+    return this.pairsGuessed == this.cards.length / 2;
   }
 }
